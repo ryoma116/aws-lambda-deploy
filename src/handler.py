7522716326ed52e6203fe3b6325685e0056c3086
ryoma116 <uma19880116>
2021-06-30 11:12:24 +0900

    Change message

--- src/handler.py
+++ src/handler.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Hello from Lambda! Auto deploy!')
     }
