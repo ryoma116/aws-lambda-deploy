e43340b52e18879407d11210e0f19fec14ba35db
ryoma116 <uma19880116>
2021-06-30 13:59:59 +0900

    test: GitActions

--- src/handler.py
+++ src/handler.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! Auto deploy!')
+        'body': json.dumps('Hello from Lambda! Auto deploy! Auto deploy!')
     }
